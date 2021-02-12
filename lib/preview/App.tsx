ae3136c1168ff6f388a5e9772789b8503b8061e4
kleo <s-kxhindoli@subsplash.com>
2021-02-12 23:44:16 +0100

    add dev mode

---
+++ lib/preview/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '..';
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <Button>This is a button</Button>
+  );
+}
+
+export default App
