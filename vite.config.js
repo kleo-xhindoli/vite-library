78e39347abefadd21b2715803dbb2f85fde59120
kleo <s-kxhindoli@subsplash.com>
2021-02-12 21:27:19 +0100

    update entry points

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ module.exports = {
   build: {
     lib: {
       entry: path.resolve(__dirname, "lib/index.ts"),
-      name: "@kleo-xhindoli/vite-library",
+      name: "vite-library",
     },
     // rollupOptions: {
     //   // make sure to externalize deps that shouldn't be bundled
