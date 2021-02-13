512ebd1914eaec6ce8b18268b385c2b15bc763db
kleo <s-kxhindoli@subsplash.com>
2021-02-13 19:11:52 +0100

    add styles to button

--- lib/components/Button.tsx
+++ lib/components/Button.tsx
@@ -3,7 +3,20 @@ import React from "react";
 interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {}
 
 const Button: React.FC<ButtonProps> = (props) => {
-  return <button {...props} />;
+  return (
+    <button
+      style={{
+        padding: "8px 16px",
+        background: "#16A34A",
+        fontWeight: "bold",
+        color: "white",
+        border: "none",
+        borderRadius: "6px",
+        cursor: "pointer",
+      }}
+      {...props}
+    />
+  );
 };
 
 export default Button;
